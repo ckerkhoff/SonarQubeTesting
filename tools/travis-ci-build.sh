225d97d76e34ccacf022a547a1618843badfd23f
Chris Kerkhoff <chris.kerkhoff@designreadycontrols.com>
2020-07-16 09:58:26 -0500

    Use new way to analyze project with SonarQube

---
+++ tools/travis-ci-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Changing to /src directory..."
+cd src
+echo "Executing MSBuild.exe begin command..."
+mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"ckerkhoff" /k:"ckerkhoff_SonarQubeTesting" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+echo "Running build..."
+msbuild /p:Configuration=Release /t:Rebuild SonarQubeIssues.sln
+#echo "Running tests..."
+#dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
+echo "Executing MSBuild.exe end command..."
+mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
